f7b440943d4ffce0cc6e3b59eb80c799a73e0259
James Mai (HaiMS) <maisonhai3@gmail.com>
2025-11-18 09:15:22 +0700

    Add LotService

--- src/auction/usecases/LotService.java
+++ src/auction/usecases/LotService.java
@@ -6,17 +6,40 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LotService {
-    public List<Lot> lotsInStorage;
+    private List<Lot> lotsInStorage;
 
     // Singleton
-    public static final LotService INSTANCE = new LotService();
+    private static final LotService INSTANCE = new LotService();
 
     private LotService() {
         this.lotsInStorage = new ArrayList<>();
     }
 
-    public LotService getInstance() {
+    public static LotService getInstance() {
         return INSTANCE;
     }
 
+    // Methods to manage lots
+    public void addLot(Lot lot) {
+        lotsInStorage.add(lot);
+    }
+
+    public List<Lot> getAllLots() {
+        return new ArrayList<>(lotsInStorage); // Return copy for safety
+    }
+
+    public Lot getLot(int index) {
+        if (index >= 0 && index < lotsInStorage.size()) {
+            return lotsInStorage.get(index);
+        }
+        return null;
+    }
+
+    public boolean removeLot(Lot lot) {
+        return lotsInStorage.remove(lot);
+    }
+
+    public int getLotCount() {
+        return lotsInStorage.size();
+    }
 }
